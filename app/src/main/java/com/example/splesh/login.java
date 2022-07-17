daae06488964f8393e8e0c0aba91189261e59a08
khushal ninave <khushal.ninave02@gmail.com>
2022-07-16 20:55:56 -0700

    test

---
+++ app/src/main/java/com/example/splesh/login.java
@@ -0,0 +1,16 @@
+package com.example.splesh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+    }
+}
